1e6c0360cf21cca234b882f4ecd3d050c310149f
jea-frvnco <163940309+jea-frvnco@users.noreply.github.com>
2024-06-27 23:09:48 -0400

    Update evidencia.tf

--- evidencia.tf
+++ evidencia.tf
@@ -15,7 +15,7 @@ provider "aws" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
  
-  name = "my-vpc"
+  name = "jean-vpc"
   cidr = "10.0.0.0/16"
  
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
@@ -30,9 +30,10 @@ module "vpc" {
     Environment = "prd"
   }
 }
+
 # Crear un Security Group
-resource "aws_security_group" "allow_traffic" {
-  name        = "allow_traffic"
+resource "aws_security_group" "jean_allow_traffic" {
+  name        = "jean_allow_traffic"
   description = "Allow traffic on ports 80, 443, and 22"
   vpc_id      = module.vpc.vpc_id
 
@@ -65,84 +66,86 @@ resource "aws_security_group" "allow_traffic" {
   }
 
   tags = {
-    Name = "allow_traffic"
+    Name = "jean_allow_traffic"
   }
 }
 
 # Crear un bucket S3 y copiar archivo index.php
-resource "aws_s3_bucket" "bucket" {
-  bucket = "my-website-bucket"
+resource "aws_s3_bucket" "jean_bucket" {
+  bucket = "jean-website-bucket"
   acl    = "private"
 
   tags = {
-    Name        = "my-website-bucket"
+    Name        = "jean-website-bucket"
     Environment = "prd"
   }
 }
 
 resource "aws_s3_bucket_object" "index_php" {
-  bucket = aws_s3_bucket.bucket.bucket
+  bucket = aws_s3_bucket.jean_bucket.bucket
   key    = "index.php"
   source = "index.php"
   acl    = "public-read"
 }
 
 # Lanzar 3 instancias EC2 en diferentes AZs
-resource "aws_instance" "web" {
+resource "aws_instance" "jean_web" {
   count         = 3
   ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   key_name      = "vockey"
 
   subnet_id = element(module.vpc.public_subnets, count.index)
-  security_groups = [aws_security_group.allow_traffic.name]
+  security_groups = [aws_security_group.jean_allow_traffic.name]
 
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
-              sudo yum install -y httpd php
+              sudo yum install -y httpd php amazon-efs-utils
               sudo systemctl start httpd
               sudo systemctl enable httpd
-              aws s3 cp s3://${aws_s3_bucket.bucket.bucket}/index.php /var/www/html/
+              sudo mkdir -p /var/www/html
+              sudo mount -t efs -o tls ${aws_efs_file_system.efs.id}:/ /var/www/html
+              aws s3 cp s3://${aws_s3_bucket.jean_bucket.bucket}/index.php /var/www/html/
               EOF
 
   tags = {
-    Name = "WebServer-${count.index}"
+    Name = "JeanWebServer-${count.index}"
   }
 }
 
 # Crear un volumen EFS y montarlo en las instancias EC2
-resource "aws_efs_file_system" "efs" {
-  creation_token = "my-efs"
+resource "aws_efs_file_system" "jean_efs" {
+  creation_token = "jean-efs"
   tags = {
-    Name = "my-efs"
+    Name = "jean-efs"
   }
 }
 
-resource "aws_efs_mount_target" "efs_mount" {
+resource "aws_efs_mount_target" "jean_efs_mount" {
   count          = 3
-  file_system_id = aws_efs_file_system.efs.id
+  file_system_id = aws_efs_file_system.jean_efs.id
   subnet_id      = element(module.vpc.public_subnets, count.index)
-  security_groups = [aws_security_group.allow_traffic.id]
+  security_groups = [aws_security_group.jean_allow_traffic.id]
 }
 
 # Crear un Load Balancer y adjuntar las instancias
-resource "aws_lb" "alb" {
-  name               = "my-alb"
+resource "aws_lb" "jean_alb" {
+  name               = "jean-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.allow_traffic.id]
+  security_groups    = [aws_security_group.jean_allow_traffic.id]
   subnets            = module.vpc.public_subnets
 
   enable_deletion_protection = false
 
   tags = {
-    Name = "my-alb"
+    Name = "jean-alb"
   }
 }
 
-resource "aws_lb_target_group" "target_group" {
-  name     = "my-targets"
+resource "aws_lb_target_group" "jean_target_group" {
+  name     = "jean-targets"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
@@ -157,24 +160,24 @@ resource "aws_lb_target_group" "target_group" {
   }
 
   tags = {
-    Name = "my-targets"
+    Name = "jean-targets"
   }
 }
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.alb.arn
+resource "aws_lb_listener" "jean_listener" {
+  load_balancer_arn = aws_lb.jean_alb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.target_group.arn
+    target_group_arn = aws_lb_target_group.jean_target_group.arn
   }
 }
 
-resource "aws_lb_target_group_attachment" "target_attachment" {
+resource "aws_lb_target_group_attachment" "jean_target_attachment" {
   count            = 3
-  target_group_arn = aws_lb_target_group.target_group.arn
-  target_id        = element(aws_instance.web.*.id, count.index)
+  target_group_arn = aws_lb_target_group.jean_target_group.arn
+  target_id        = element(aws_instance.jean_web.*.id, count.index)
   port             = 80
 }
